bbf15d4c07f1da861b1aa571dd9137d4c5304ab7
suhelhusain <suhelhusain7860@gmail.com>
2026-08-29 12:33:07 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,46 @@
+//package DSAinJAVA;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+      /*   int marks[] = new int[100];
+        Scanner sc  = new Scanner(System.in);
+      //  System.out.println("lenght of arrays = "+marks.length);
+  //int phy
+  // int chem
+  marks[0] = sc.nextInt();//phy
+  marks[1]  = sc.nextInt();  //chem
+  marks[2] = sc.nextInt();//math
+  System.out.println("phy:"+marks[0] );
+  System.out.println("chem:"+marks[1] );
+  System.out.println("math:"+marks[2] );
+  
+
+  marks[2] = marks[2]+1;
+  System.out.println("math:" +marks[2]);
+  int percentage = (marks[0] + marks[1] + marks[3])/3;
+  System.out.println("Percentage =" + percentage+"%");
+  /* */   
+
+
+  int marks[] = new int[80];
+  Scanner sc = new Scanner(System.in);
+  marks[0] = sc.nextInt();//computer
+  marks[1] = sc.nextInt();//softer engineering
+  marks[2] = sc.nextInt();// DBMS
+  System.out.println("computer:" +marks[0]);
+  System.out.println("softer engineering :" +marks[1]);
+  System.out.println("DBMS :"+marks[2]);
+  marks[2] =marks[2]+5;
+  System.out.println("DBMS :"+marks[2]);
+  int percentage = (marks[0]+marks[1]+marks[2])/3;
+  System.out.println("percentage="+percentage+"%");
+
+
+  
+
+}
+    
+    
+}
