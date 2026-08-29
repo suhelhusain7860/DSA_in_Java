e858d6d3fa6df0478e98a1b6a433458f34bb33fd
suhelhusain <suhelhusain7860@gmail.com>
2026-08-29 12:38:57 +0530

    Add files via upload

---
+++ function_update.java
@@ -0,0 +1,44 @@
+//package DSAinJAVA.arrays;
+
+public class function_update {
+    // public static void update(int marks[], int nonChangable) {
+    //     nonChangable = 10;
+    //     for(int i=0; i<marks.length; i++){
+    //         marks[i] = marks[i]+1;
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     int marks[] = {97, 98,99};
+    //     int nonChangable = 5;
+    //     update(marks,nonChangable);
+    //     System.out.println(nonChangable);
+    //     //update(marks);
+    //     update(marks,nonChangable);
+    //     System.out.println(nonChangable);
+    //     //print our marks
+    //     for(int i=0; i<marks.length; i++){
+    //         System.out.println(marks[i]+" ");
+    //     }
+    //     System.out.println();
+    public static void update(int marks[], int nonChangable){
+        nonChangable = 10;
+        for(int i=0; i<marks.length; i++){
+            marks[i] = marks[i]+1;
+
+        }
+    }
+    public static void main(String[] args) {
+        int marks[] = {97,98,99};
+        int nonChangable  =5;
+        update(marks, nonChangable);
+        System.out.println(nonChangable);
+        //update(marks)
+        update(marks, nonChangable);
+        for(int i=0; i<marks.length; i++){
+            System.out.println(marks[i]+1);
+        }
+        System.out.println();
+    }
+    }
+    
+
